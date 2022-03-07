94945d553f21e35e1220257345c9b769205028fc
AuroraTejada <daw13.2022@gmail.com>
2022-03-07 13:26:12 +0100

    Proyecto Documentado

--- Hora.java
+++ Hora.java
@@ -1,10 +1,26 @@
 package examen;
-
+/**
+ * Clase hora que sirve para comprobar si una hora es correcta o no
+ * @author DAW113
+ *
+ */
 public class Hora {
+	/**
+	 * Atributo público de la clase hora, con el cual verificamos si la hora es correcta o no. Y que hemos inicializado en el metodo validarHota
+	 */
 	public static boolean horaValida;
-
+/**
+ * Método público que devuelve un booleano y que usamos para verificar si una hora es valida o no
+ * @param segundos
+ * @param minutos
+ * @param horas
+ * @return horaValida (true o false)
+ */
 	public static boolean validarHora(int segundos, int minutos, int horas) {
 		horaValida = true;
+		/**
+		 * Condiciones que se utilizan para verificar si es correcta o no la hora
+		 */
 		if ((horas >= 0) && (minutos >= 0) && (segundos >= 0)) {
 			if (horas > 23) {
 				horaValida = false;
